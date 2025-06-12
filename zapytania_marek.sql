d162fd387760a0527dcee522dbfd00bd461b3176
Marek Dworaczyk <275996@student.pwr.edu.pl>
2025-06-12 18:40:39 +0200

    update: pomysły analizy

--- zapytania_marek.sql
+++ zapytania_marek.sql
@@ -1,7 +1,4 @@
 SELECT
-    tp.client_id,
-    tp.trip_id,
-    t.departure_datetime
-FROM trip_participants tp
-JOIN trips t ON tp.trip_id = t.trip_id
-ORDER BY tp.client_id, t.departure_datetime;
+*
+FROM trips t
+WHERE status LIKE "completed"
