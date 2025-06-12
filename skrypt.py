30d22b8ea2c501b724ad7cd08a9066ca91432d5d
mathieuboro <mqt3usz@gmail.com>
2025-06-12 23:04:55 +0200

    poprawki

--- skrypt.py
+++ skrypt.py
@@ -681,7 +681,7 @@ def uruchom():
             continue
 
         if trip_id in trips_with_incidents:
-            rating = random.choices([1,2,3,4,5], weights=[0.15, 0.3, 0.4, 0.15, 0.1])[0]
+            rating = random.choices([1,2,3,4,5], weights=[0.1, 0.25, 0.3, 0.2, 0.15])[0]
         else:
             rating = random.choices([1,2,3,4,5], weights=[0.05, 0.1, 0.15, 0.3, 0.4])[0]
 
