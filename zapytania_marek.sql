a7c4b29e4a9f1550d32ba6b24363a8977cc8b76c
Marek Dworaczyk <275996@student.pwr.edu.pl>
2025-06-14 14:04:20 +0200

    update: koniec analizy, pora na tekst

--- zapytania_marek.sql
+++ zapytania_marek.sql
@@ -1,10 +1,1 @@
-SELECT
-e.employee_id,
-COUNT(*) AS success
-FROM trips t
-JOIN feedback f ON f.trip_id = t.trip_id
-JOIN employee_assignments ea ON ea.trip_id = t.trip_id
-JOIN employees e ON ea.employee_id = e.employee_id
-WHERE rating > 3
-GROUP BY ea.employee_id
-ORDER BY success DESC
+SELECT * from trips
